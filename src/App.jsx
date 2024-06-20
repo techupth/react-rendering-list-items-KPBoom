e8fbdf24a865d99bff6a2a10e435d8244e427e49
Boom <boom2k31@gmail.com>
2024-06-20 15:59:12 +0700

    Finish Assign

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import "./App.css";
+import movies from "./data/movies.jsx"
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
-        {/* Render Movie Lists Here */}
+        <div className="main-page">
+          <h1>Movie List Section</h1>
+          {
+            movies.map((movie,index)=>{
+              return  (
+              <div className="movie-card" key={index}>
+                <img src={movie.image} alt="image" className="movie-image" />
+                <div className="movie-detail">
+                   <p>Title: {movie.title}</p>
+                   <p>Year: {movie.year}</p>
+                   <p>Runtime: {movie.runtime}</p>
+                   <p>Genres:<span className="hi-light"> {movie.genres} </span></p>
+                   <p>IMDB Ratings: {movie.imdbRating}</p>
+                   <p>IMDB Votes: {movie.imdbVotes}</p>
+                </div>
+              </div>
+              )
+            })
+          }
+        </div>
       </section>
     </div>
   );
